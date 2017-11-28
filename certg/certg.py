6a99019013afb24ec074ed5d381b0fc825eb5375
Facundo Batista <facundo@taniquetil.com.ar>
2017-11-28 12:58:58 -0300

    Refactored into a lib.

---
+++ certg/certg.py
@@ -0,0 +1,62 @@
+# Copyright 2013-2017 Facundo Batista
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For further info, check  http://github.com/facundobatista/certg
+
+import os
+import subprocess
+import tempfile
+
+
+def process(svg_source, result_prefix, result_distinct, replace_info, progress_cb=None):
+    """Generate N PDFs.
+
+    Each PDF is from a key in replace_info, replacing data into the
+    svg_source, and naming each PDF according to result_*.
+
+    After each PDF progress_cb (if any) will be called to indicate progress.
+    """
+    with open(svg_source, "rt", encoding='utf8') as fh:
+        content_base = fh.read()
+
+    # get all the replacing attrs
+    replacing_attrs = set()
+    for data in replace_info:
+        replacing_attrs.update(data)
+
+    for data in replace_info:
+        # indicate advance
+        progress_cb()
+
+        # replace content
+        content = content_base
+        for attr in replacing_attrs:
+            value = data.get(attr)
+            if value is None:
+                # both because the attr is not supplied, or supplied empty
+                value = ""
+            content = content.replace("{{" + attr + "}}", value)
+
+        # write the new svg
+        _, tmpfile = tempfile.mkstemp(suffix='.svg')
+        with open(tmpfile, "wt", encoding='utf8') as fh:
+            fh.write(content)
+
+        # generate PDF
+        distinct = data[result_distinct].lower().replace(" ", "")
+        result = "{}-{}.pdf".format(result_prefix, distinct)
+
+        cmd = ['inkscape', '--export-text-to-path', '--export-pdf={}'.format(result), tmpfile]
+        subprocess.check_call(cmd)
+        os.remove(tmpfile)
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Copyright 2017 Facundo Batista
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For further info, check  http://github.com/facundobatista/certg
+
+"""Set up certg."""
+
+from distutils.core import setup
+
+setup(
+    name='certg',
+    version='3.0',
+    license='GPL-3',
+    author='Facundo Batista',
+    author_email='facundo@taniquetil.com.ar',
+    description='A certificate generator, from a SVG to a lot of PDFs',
+    long_description=open('README.rst').read(),
+    url='https://github.com/facundobatista/certg',
+
+    packages=["certg"],
+    scripts=["bin/certg"],
+
+    install_requires=['progress', 'pyyaml'],
+)
